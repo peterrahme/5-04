56b3136854f0150ab32c01d11f5a3a63c810551b
peterrahme <peter.rahme@ocsbstudent.ca>
2021-02-12 14:44:11 +0000

    I added a space in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('show').addEventListener('click',reportResults)
+document.getElementById('show').addEventListener('click', reportResults)
 
 /* This code allows me to have a box where whatever I type in is shown by alert on the top of the screen. This happens when I click the button that says Show Results which also changes the text on the webpage */
 function reportResults () {
